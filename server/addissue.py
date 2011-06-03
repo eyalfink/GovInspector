1622770729a3769c79ecd8b0403f6069723b1df6
Eyal Fink <eyalf@eyalf-glaptop.corp.google.com>
2011-06-03 15:43:34 +0300

    some more examples

--- server/addissue.py
+++ server/addissue.py
@@ -7,6 +7,8 @@ class Main(utils.Handler):
     def get(self):
         self.render('templates/hello.html',
                     foo='bar')
+    def post(self):
+        
 
 
 if __name__ == '__main__':
