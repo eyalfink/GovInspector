95befdc893129bc84c1e02b451fda69e50d6bdc3
Gil Silas (macbook) <gil.silas@gmail.com>
2011-06-03 17:23:18 +0300

    add issue form

--- server/add_issue.py
+++ server/add_issue.py
@@ -1,13 +1,11 @@
 import utils
 
-class Main(utils.Handler):
+class AddIssue(utils.Handler):
     def get(self):
-        self.render('templates/hello.html',
-                    foo='bar')
-    def post(self):
-        pass
+        self.render('templates/add_issue.html',
+                    params=self.params)
 
 
 if __name__ == '__main__':
-    utils.run([('/', Main)], debug=True)
+    utils.run([('/admin/add_issue', AddIssue)], debug=True)
 
