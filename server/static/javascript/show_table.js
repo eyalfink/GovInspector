7e281035bd009b7d30ee1ec9567a4f1e8a5ba7c5
Eyal Fink <eyal.fink@gmail.com>
2011-06-04 18:22:35 +0300

    Add the three widgets to the page, no events yet

--- server/static/javascript/show_table.js
+++ server/static/javascript/show_table.js
@@ -1,19 +1,76 @@
 // -*- coding: utf-8 -*-
 
-google.load("visualization", "1", {packages: ["table"]});
-google.setOnLoadCallback(drawFusionTable);
+google.load("visualization", "1", {packages: ["table", "corechart"]});
+google.setOnLoadCallback(createDashboard);
+
+var gii = gii || {};
+gii.FT_URL = 'http://www.google.com/fusiontables/gvizdata?tq=';
+gii.TABLE_NUMBER = 946168;
+
+function createDashboard() {
+    createOfficeTable();
+    createPerformanceChart();
+    createIssuesTable();
+}
 
 
-function drawFusionTable() {
-    var queryText = encodeURIComponent("SELECT text, topic, report, status FROM 946168");
-    var query = new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq='  + queryText);
+////////////////////////////////////////////////////////////
+//   Office list table
+////////////////////////////////////////////////////////////
+function createOfficeTable() {
+    var queryText = encodeURIComponent(
+        "SELECT office, count(status) FROM " + gii.TABLE_NUMBER + " GROUP BY office");
+    var query = new google.visualization.Query(gii.FT_URL  + queryText);
   
     query.send(function(response) {
-	    drawTable(response.getDataTable());
+	    drawOfficeTable(response.getDataTable());
 	});
 }
 
-function drawTable(data) {
+function drawOfficeTable(data) {
+    var tableEl = document.getElementById('gii_office_list_view')
+    var table = new google.visualization.Table(tableEl);
+    table.draw(data,{
+	width           : '40%',
+	rtlTable: true});
+}
+
+////////////////////////////////////////////////////////////
+//   Office performance chart
+////////////////////////////////////////////////////////////
+function createPerformanceChart() {
+    var queryText = encodeURIComponent(
+        "SELECT report, average(status) FROM " + gii.TABLE_NUMBER + " GROUP BY report");
+    var query = new google.visualization.Query(gii.FT_URL  + queryText);
+  
+    query.send(function(response) {
+	    drawPerformanceChart(response.getDataTable());
+	});
+}
+
+function drawPerformanceChart(data) {
+    var chartEl = document.getElementById('gii_performance_view');
+    var chart = new google.visualization.BarChart(chartEl);
+    chart.draw(data, {
+    });
+
+}
+
+
+////////////////////////////////////////////////////////////
+//   Issues table
+////////////////////////////////////////////////////////////
+function createIssuesTable() {
+    var queryText = encodeURIComponent(
+        "SELECT text, topic, report, status FROM " + gii.TABLE_NUMBER);
+    var query = new google.visualization.Query(gii.FT_URL  + queryText);
+  
+    query.send(function(response) {
+	    drawIssuesTable(response.getDataTable());
+	});
+}
+
+function drawIssuesTable(data) {
     var tableEl = document.getElementById('gii_issues_list_view')
     var table = new google.visualization.Table(tableEl);
     table.draw(data, {showRowNumber: true,
