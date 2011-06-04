420fa52f447127552cbb9447dc1ee16d8c9cc8b7
Ory Band <oryband@gmail.com>
2011-06-04 16:44:07 +0300

    renamed report_by_office to ministries.

--- server/ministries.py
+++ server/ministries.py
@@ -5,10 +5,10 @@ import model
 
 class ReportByOffice(utils.Handler):
     def get(self):
-        self.render('templates/report_by_office.html',
+        self.render('templates/ministries.html',
                     params=self.params)
 
 
 if __name__ == '__main__':
-    utils.run([('/report_by_office', ReportByOffice)], debug=True)
+    utils.run([('/ministries', ReportByOffice)], debug=True)
 
